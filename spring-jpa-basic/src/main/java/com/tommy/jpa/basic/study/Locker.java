2aad4197349e9b585e9ba495bd04a2811ae8e618
임한결 <dlagksruf19@naver.com>
2021-04-07 17:26:50 +0900

    [Section6. 다양한 연관관계 매핑] 일대일 [1:1]
    일대일 연관관계의 경우 주 테이블과 대상 테이블 중 어떤 것을 기준으로 잡을지에 대해서 이슈가 있다.
    프로젝트 예시의 경우 Member에서 외래키로 Locker를 가지고,
    Member를 주테이블로 잡는 것을 선호하지만, 업무에서는 협의 필요성이 있다.

---
+++ spring-jpa-basic/src/main/java/com/tommy/jpa/basic/study/Locker.java
@@ -0,0 +1,18 @@
+package com.tommy.jpa.basic.study;
+
+import javax.persistence.*;
+
+@Entity
+public class Locker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "LOCKER_ID")
+    private Long id;
+
+    private String name;
+
+    // 양방향의 경우
+    @OneToOne(mappedBy = "locker")
+    private Member member;
+}
