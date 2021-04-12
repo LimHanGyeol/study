599e4df39fd39fd6a0afa73efbc843ca68a6d1eb
임한결 <dlagksruf19@naver.com>
2021-04-12 16:37:37 +0900

    [Section11. 객체지향 쿼리 언어2 - 중급 문법] 엔티티 직접 사용

--- spring-jpa-basic/src/main/java/com/tommy/jpa/basic/jpql/JpqlMain.java
+++ spring-jpa-basic/src/main/java/com/tommy/jpa/basic/jpql/JpqlMain.java
@@ -20,6 +20,13 @@ public class JpqlMain {
         transaction.begin();
 
         try {
+            // Fetch Join
+            // fetchJoin(entityManager);
+
+            // 엔티티 직접 사용
+            // entity(entityManager, member1);
+
+
             TeamJ teamA = new TeamJ("개발팀");
             entityManager.persist(teamA);
 
@@ -41,22 +48,6 @@ public class JpqlMain {
             entityManager.flush();
             entityManager.clear();
 
-            // ManyToOne 관계 Fetch Join
-            String query = "SELECT m FROM MemberJ AS m JOIN FETCH m.teamJ";
-
-            // Collection Fetch Join. 일대다 관계
-            // 컬렉션 페치 조인을 하면 일대다 조인이기 때문에 모든 데이터를 엘리먼트 만큼 가지고 온다.
-            //  WHERE t.name = '개발팀'
-            String collectionFetchJoinQuery = "SELECT DISTINCT t FROM TeamJ AS t JOIN FETCH t.members";
-            List<TeamJ> results = entityManager.createQuery(collectionFetchJoinQuery, TeamJ.class)
-                    .getResultList();
-
-            for (TeamJ result : results) {
-                System.out.println("result = " + result + ", " + result.getMembers().size());
-                for (MemberJ member : result.getMembers()) {
-                    System.out.println(" -> member = " + member);
-                }
-            }
 
             transaction.commit();
         } catch (Exception e) {
@@ -65,5 +56,40 @@ public class JpqlMain {
             entityManager.close();
         }
         entityManagerFactory.close();
+    }
+
+    private static void entity(EntityManager entityManager, MemberJ member1) {
+        // 엔티티 직접 사용
+        // 엔티티로 조회할 경우 엔티티의 기본 키로 SELECT SQL 이 나간다.
+        // String query = "SELECT m FROM MemberJ AS m WHERE m = :member";
+        String query = "SELECT m FROM MemberJ AS m WHERE m.id = :memberId";
+
+        // 외래키 직접 사용은 외래키 FK인 TEAM_ID와 매핑 된다.
+        String fkQuery = "SELECT m FROM MemberJ AS m WHERE m.teamJ = :team";
+        MemberJ findMember = entityManager.createQuery(query, MemberJ.class)
+                .setParameter("memberId", member1.getId())
+                .getSingleResult();
+//                    .setParameter("member", member1)
+
+        System.out.println("findMember = " + findMember);
+    }
+
+    private static void fetchJoin(EntityManager entityManager) {
+        // ManyToOne 관계 Fetch Join
+        String query = "SELECT m FROM MemberJ AS m JOIN FETCH m.teamJ";
+
+        // Collection Fetch Join. 일대다 관계
+        // 컬렉션 페치 조인을 하면 일대다 조인이기 때문에 모든 데이터를 엘리먼트 만큼 가지고 온다.
+        //  WHERE t.name = '개발팀'
+        String collectionFetchJoinQuery = "SELECT DISTINCT t FROM TeamJ AS t JOIN FETCH t.members";
+        List<TeamJ> results = entityManager.createQuery(collectionFetchJoinQuery, TeamJ.class)
+                .getResultList();
+
+        for (TeamJ result : results) {
+            System.out.println("result = " + result + ", " + result.getMembers().size());
+            for (MemberJ member : result.getMembers()) {
+                System.out.println(" -> member = " + member);
+            }
+        }
     }
 }
