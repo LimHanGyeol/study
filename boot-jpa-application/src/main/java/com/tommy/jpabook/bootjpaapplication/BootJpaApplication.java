5c11b9f86de4e253d389704b9e58c409eeaa1686
임한결 <dlagksruf19@naver.com>
2021-04-21 15:32:39 +0900

    [Section1. 프로젝트 환경설정] 프로젝트 생성

---
+++ boot-jpa-application/src/main/java/com/tommy/jpabook/bootjpaapplication/BootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.jpabook.bootjpaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJpaApplication.class, args);
+    }
+
+}
