e0f77f482cfb109893b5af0136490c9be9474249
임한결 <dlagksruf19@naver.com>
2021-04-21 17:32:52 +0900

    [Section1. 프로젝트 환경설정] JPA와 DB 설정, 동작확인

---
+++ boot-jpa-application/src/main/java/com/tommy/jpabook/bootjpaapplication/member/MemberRepository.java
@@ -0,0 +1,22 @@
+package com.tommy.jpabook.bootjpaapplication.member;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Long save(Member member) {
+        entityManager.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return entityManager.find(Member.class, id);
+    }
+}
---
+++ boot-jpa-application/src/test/java/com/tommy/jpabook/bootjpaapplication/member/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.tommy.jpabook.bootjpaapplication.member;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("Member 생성 테스트")
+    @Rollback(value = false)
+    void testMember() {
+        // given
+        Member member = new Member("hangyeol");
+
+        // when
+        Long savedMemberId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedMemberId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(savedMemberId);
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
