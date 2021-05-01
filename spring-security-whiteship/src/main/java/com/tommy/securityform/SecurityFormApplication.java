168b57ec5cad779dc086e2bff7adffba6378e60a
임한결 <dlagksruf19@naver.com>
2021-05-01 18:04:40 +0900

    Initial project - spring security whiteship

---
+++ spring-security-whiteship/src/main/java/com/tommy/securityform/SecurityFormApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.securityform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityFormApplication.class, args);
+    }
+
+}
---
+++ spring-security-whiteship/src/test/java/com/tommy/securityform/SecurityFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.securityform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
