cfafcab0f42a8441075a4939559fa8b1ef104170
임한결 <dlagksruf19@naver.com>
2021-05-10 01:08:06 +0900

    Initial project - Spring Data JPA

---
+++ spring-data-jpa/src/main/java/com/tommy/datajpa/DataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
+
+}
