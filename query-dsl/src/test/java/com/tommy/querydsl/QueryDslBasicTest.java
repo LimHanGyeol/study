c4ce68a20ab5e65a418f30cfc7585a52c0139828
limhangyeol <dlagksruf19@naver.com>
2021-05-13 21:53:43 +0900

    [Section3. 기본 문법] 정렬

--- query-dsl/src/test/java/com/tommy/querydsl/QueryDslBasicTest.java
+++ query-dsl/src/test/java/com/tommy/querydsl/QueryDslBasicTest.java
@@ -161,4 +161,31 @@ public class QueryDslBasicTest {
                 .selectFrom(member)
                 .fetchCount();
     }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순 (DESC)
+     * 2. 회원 이름 올림차순 (ASC)
+     * 단, 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    void sort() {
+        entityManager.persist(new Member(null, 100));
+        entityManager.persist(new Member("member5", 100));
+        entityManager.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
 }
