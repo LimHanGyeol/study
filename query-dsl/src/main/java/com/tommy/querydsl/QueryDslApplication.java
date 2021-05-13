297f823cb980e113b751b9f233542655a0eb3d41
limhangyeol <dlagksruf19@naver.com>
2021-05-13 15:31:25 +0900

    Initial project Query-DSL

---
+++ query-dsl/src/main/java/com/tommy/querydsl/QueryDslApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryDslApplication.class, args);
+    }
+
+}
---
+++ query-dsl/src/test/java/com/tommy/querydsl/QueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
