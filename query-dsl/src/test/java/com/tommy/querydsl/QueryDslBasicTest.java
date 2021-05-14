b0258b09ac9f5a8ea00a722556b6917b07ab9b99
limhangyeol <dlagksruf19@naver.com>
2021-05-14 11:30:15 +0900

    [Section3. 기본 문법] Case 문
    조건을 연산하는 쿼리를 짜지 말고, row 데이터를 가져와서 Application에서 연산하는 것을 권장한다.

--- query-dsl/src/test/java/com/tommy/querydsl/QueryDslBasicTest.java
+++ query-dsl/src/test/java/com/tommy/querydsl/QueryDslBasicTest.java
@@ -2,6 +2,7 @@ package com.tommy.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tommy.querydsl.member.Member;
@@ -458,8 +459,38 @@ public class QueryDslBasicTest {
                 .from(member)
                 .fetch();
 
-        for (Tuple tuple :  result) {
+        for (Tuple tuple : result) {
             System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    void basicCase() {
+        List<String> result = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String print : result) {
+            System.out.println("print = " + print);
+        }
+    }
+
+    @Test
+    void complexCase() {
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String print : result) {
+            System.out.println("print = " + print);
         }
     }
 }
