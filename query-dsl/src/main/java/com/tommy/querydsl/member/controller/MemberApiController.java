cf9fa50b74e3dcc67506d4871960753a2c20c6e0
limhangyeol <dlagksruf19@naver.com>
2021-05-16 19:00:06 +0900

    [Section6. Spring Data JPA와 QueryDSL] Spring Data Paging 활용3 - Controller 개발

--- query-dsl/src/main/java/com/tommy/querydsl/member/controller/MemberApiController.java
+++ query-dsl/src/main/java/com/tommy/querydsl/member/controller/MemberApiController.java
@@ -1,9 +1,12 @@
 package com.tommy.querydsl.member.controller;
 
 import com.tommy.querydsl.member.domain.MemberJpaRepository;
+import com.tommy.querydsl.member.domain.MemberRepository;
 import com.tommy.querydsl.member.dto.MemberSearchCondition;
 import com.tommy.querydsl.member.dto.MemberTeamDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +17,20 @@ import java.util.List;
 public class MemberApiController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
