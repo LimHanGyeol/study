77ebe4b5e5bdd2be82e9210b4cafb6fd10124c34
limhangyeol <dlagksruf19@naver.com>
2021-06-17 12:05:07 +0900

    Initial project - boot restful

---
+++ boot-restful/src/main/java/com/tommy/bootrestful/BootRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.bootrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRestfulApplication.class, args);
+    }
+
+}
---
+++ boot-restful/src/test/java/com/tommy/bootrestful/BootRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.bootrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
