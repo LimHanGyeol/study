8cd4128d880ec2dbcd558b046a78184f63177c24
limhangyeol <dlagksruf19@naver.com>
2021-06-19 15:04:15 +0900

    [Section2. User Service API 구현] HTTP Status Code 제어

--- boot-restful/src/main/java/com/tommy/bootrestful/user/controller/UserController.java
+++ boot-restful/src/main/java/com/tommy/bootrestful/user/controller/UserController.java
@@ -3,8 +3,11 @@ package com.tommy.bootrestful.user.controller;
 import com.tommy.bootrestful.user.domain.User;
 import com.tommy.bootrestful.user.service.UserDaoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -24,7 +27,13 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
