13614862532dc7794a99f60ae24472796e6ec8f2
limhangyeol <dlagksruf19@naver.com>
2021-06-26 00:24:47 +0900

    [Section4. Spring Boot API 사용] Spring Security를 이용한 인증 처리

---
+++ boot-restful/src/main/java/com/tommy/bootrestful/common/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.tommy.bootrestful.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("tommy")
+                .password("{noop}test1234")
+                .roles("USERS");
+    }
+}
