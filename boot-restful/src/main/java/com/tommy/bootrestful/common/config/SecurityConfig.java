fc6c42f0b09d3e2ff499474fc05cee79b1d0b4b1
limhangyeol <dlagksruf19@naver.com>
2021-06-26 22:20:49 +0900

    [Section5. Java Persistence API 사용] JPA 사용을 위한 Dependency 추가와 설정

--- boot-restful/src/main/java/com/tommy/bootrestful/common/config/SecurityConfig.java
+++ boot-restful/src/main/java/com/tommy/bootrestful/common/config/SecurityConfig.java
@@ -2,10 +2,23 @@ package com.tommy.bootrestful.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .and()
+                .headers()
+                .frameOptions().disable();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
