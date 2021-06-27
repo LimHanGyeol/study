072c7ce8ce854a628d5e42881fa74d29dd973347
limhangyeol <dlagksruf19@naver.com>
2021-06-27 19:33:21 +0900

    [Section5. Java Persistence API 사용] JPA를 이용한 사용자 목록 조회 - GET HTTP Method

--- boot-restful/src/main/java/com/tommy/bootrestful/user/controller/UserJpaController.java
+++ boot-restful/src/main/java/com/tommy/bootrestful/user/controller/UserJpaController.java
@@ -2,22 +2,39 @@ package com.tommy.bootrestful.user.controller;
 
 import com.tommy.bootrestful.user.domain.User;
 import com.tommy.bootrestful.user.domain.UserRepository;
+import com.tommy.bootrestful.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/jpa")
 @RestController
 public class UserJpaController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @GetMapping("/users")
     public List<User> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> findUserById(@PathVariable long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(String.format("ID[%d] not found", id)));
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).findAllUsers());
+        entityModel.add(linkTo.withRel("all-users"));
+        return entityModel;
     }
 }
