7e290dbd9a5616ef7c5a7189db221f701a92fd49
limhangyeol <dlagksruf19@naver.com>
2021-07-05 11:40:06 +0900

    Initial project - boot restful whiteship

---
+++ boot-restful-whiteship/src/main/java/com/tommy/bootrest/BootRestApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.bootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRestApplication.class, args);
+    }
+
+}
---
+++ boot-restful-whiteship/src/test/java/com/tommy/bootrest/BootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.bootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
