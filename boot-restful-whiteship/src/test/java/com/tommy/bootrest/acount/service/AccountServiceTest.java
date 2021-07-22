9ede00e42750d27d858db7354928b3eedd5a9daf
limhangyeol <dlagksruf19@naver.com>
2021-07-22 10:12:16 +0900

    [Section5. REST API 보안 적용] 예외 테스트

--- boot-restful-whiteship/src/test/java/com/tommy/bootrest/acount/service/AccountServiceTest.java
+++ boot-restful-whiteship/src/test/java/com/tommy/bootrest/acount/service/AccountServiceTest.java
@@ -3,16 +3,19 @@ package com.tommy.bootrest.acount.service;
 import com.tommy.bootrest.acount.domain.Account;
 import com.tommy.bootrest.acount.domain.AccountRepository;
 import com.tommy.bootrest.acount.domain.AccountRole;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -42,5 +45,16 @@ class AccountServiceTest {
 
         // then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    @DisplayName("존재하지 않은 유저 정보 조회 시 예외 발생")
+    void loadUserByUsernameFail() {
+        // given
+        String username = "anonymous@email.com";
+
+        // when & then
+        assertThatExceptionOfType(UsernameNotFoundException.class)
+                .isThrownBy(() -> accountService.loadUserByUsername(username));
     }
 }
