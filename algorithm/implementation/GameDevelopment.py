d78220d5cd6d86eb500948620cf118c703aa9c6a
limhangyeol <hang19663@gmail.com>
2023-12-22 22:42:54 +0900

    Algorithm(implementation): 게임 개발

---
+++ algorithm/implementation/GameDevelopment.py
@@ -0,0 +1,63 @@
+# 게임 개발
+# N, M을 공백으로 구분하여 입력 받기
+n, m = map(int, input().split())
+
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+# 현재 캐릭터의 x 좌표, y 좌표, 방향을 입력받기
+x, y, direction = map(int, input().split())
+
+d[x][y] = 1  # 현재 좌표 방문 처리
+
+# 전체 맵 정보를 입력받기
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
+# 북 동 남 서 방향 정의
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+# 왼쪽으로 회전
+def turn_left():
+    global direction  # 정수형 변수인 direction이 함수 밖에서 선언된 전역 변수이기 때문에 global 사용
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+
+# 시뮬레이션 시작
+count = 1
+turn_time = 0
+while True:
+    # 왼쪽으로 회전
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time += 1
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동하기
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
+        else:
+            break
+        turn_time = 0
+
+# 정답 출력
+print(count)
